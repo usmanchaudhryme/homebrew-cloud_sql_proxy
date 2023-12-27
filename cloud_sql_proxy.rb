557ae7ecb72bdfb0777046c0da44efeee14eabcf
Usman Chaudhry <musmanc@outlook.com>
2023-12-27 21:43:38 +0500

    Use older version

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -1,7 +1,7 @@
 class CloudSqlProxy < Formula
   desc "The Cloud SQL Proxy for GoogleCloudPlatform"
   homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
-  version "2.7.2"
+  version "1.20.2"
   url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/refs/tags/v#{version}.tar.gz"
   sha256 "665e8abc91a7496a784a2e6c723d676517072ebb22171beb5502135a3ceb1d9c"
   head "https://github.com/GoogleCloudPlatform/cloud-sql-proxy.git"
