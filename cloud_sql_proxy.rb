2d33814558d85499e106a8012493f8a60aa8c199
Usman Chaudhry <musmanc@outlook.com>
2023-12-27 22:01:29 +0500

    Bring back sha

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -3,6 +3,7 @@ class CloudSqlProxy < Formula
   homepage 'https://github.com/GoogleCloudPlatform/cloudsql-proxy'
   version '1.20.2'
   url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/refs/tags/v#{version}.tar.gz"
+  sha256 '42219fbd8fcae1cd0e393b1395178f358faa5dcea4ebe8de57fa9cb0800ed0c7'
   head 'https://github.com/GoogleCloudPlatform/cloud-sql-proxy.git'
 
   depends_on 'go' => :build
