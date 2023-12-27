3d4e689d1e4c3f4a60559b5c7026b72524c81bcd
Usman Chaudhry <musmanc@outlook.com>
2023-12-27 21:56:16 +0500

    Hash issue

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -1,23 +1,22 @@
 class CloudSqlProxy < Formula
-  desc "The Cloud SQL Proxy for GoogleCloudPlatform"
-  homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
-  version "1.20.2"
+  desc 'The Cloud SQL Proxy for GoogleCloudPlatform'
+  homepage 'https://github.com/GoogleCloudPlatform/cloudsql-proxy'
+  version '1.20.2'
   url "https://github.com/GoogleCloudPlatform/cloud-sql-proxy/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "42219fbd8fcae1cd0e393b1395178f358faa5dcea4ebe8de57fa9cb0800ed0c7"
-  head "https://github.com/GoogleCloudPlatform/cloud-sql-proxy.git"
+  head 'https://github.com/GoogleCloudPlatform/cloud-sql-proxy.git'
 
-  depends_on "go" => :build
+  depends_on 'go' => :build
 
   def install
-    system "go", "build", "-o", bin/"cloud_sql_proxy", "."
+    system 'go', 'build', '-o', bin / 'cloud_sql_proxy', '.'
   end
 
   service do
-    run [opt_bin/"cloud_sql_proxy", "-dir=/tmp/cloudsql"]
+    run [opt_bin / 'cloud_sql_proxy', '-dir=/tmp/cloudsql']
     keep_alive true
   end
 
   test do
-    system "cloud_sql_proxy -version"
+    system 'cloud_sql_proxy -version'
   end
 end
